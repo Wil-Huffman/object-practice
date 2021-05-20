47bc54484be9aaedfae95f30b9b7b24547bf7cb6
Wil-Huffman <me@wil-huffman.io>
2021-05-19 21:40:24 -0500

    Fixed formatting

--- object-examples.js
+++ object-examples.js
@@ -6,12 +6,13 @@ var car = {name: "Honda",
   isMaintained: true,
   isInsured: true,
   fasterThanLight: false,
-  extraFeatures: ['annoying headlights', 'horn always on', 'sounds like lawnmower']}
+  extraFeatures: ['annoying headlights', 'horn always on', 'sounds like lawnmower'],
+}
 
 var otter = {
   model: "river otter",
   isInsanelyCute: true,
   likesPredators: false,
   numberOfPaws: "4",
-  likesToEat: ['clams', 'little fish', 'oysters']
+  likesToEat: ['clams', 'little fish', 'oysters'],
 }
