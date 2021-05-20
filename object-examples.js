10390e415f8665805b6f7f4c57a1a8dadaaeea07
Wil-Huffman <me@wil-huffman.io>
2021-05-19 21:28:41 -0500

    Add Animal

---
+++ object-examples.js
@@ -0,0 +1,17 @@
+var car = {name: "Honda",
+  make: "accord",
+  model: "exl",
+  vin: "5254262466246246",
+  milage: "127000",
+  isMaintained: true,
+  isInsured: true,
+  fasterThanLight: false,
+  extraFeatures: ['annoying headlights', 'horn always on', 'sounds like lawnmower']}
+
+var otter = {
+  model: "river otter",
+  isInsanelyCute: true,
+  likesPredators: false,
+  numberOfPaws: "4",
+  likesToEat: ['clams', 'little fish', 'oysters']
+}
