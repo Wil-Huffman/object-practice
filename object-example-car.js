b95e0e90f054938a4e2c2433fba0bb9123120d7a
Wil-Huffman <me@wil-huffman.io>
2021-05-19 21:01:00 -0500

    Add key:values

---
+++ object-example-car.js
@@ -0,0 +1,9 @@
+var car = {name: "Honda",
+  make: "accord",
+  model: "exl",
+  vin: "5254262466246246",
+  milage: "127000",
+  isMaintained: true,
+  isInsured: true,
+  fasterThanLight: false,
+  extraFeatures: ['annoying headlights', 'horn always on', 'sounds like lawnmower'}
